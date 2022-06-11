9865335baabbf1c6a9b583dc842ea7d9d75a6494
ssw2570 <sinsang2570@gmail.com>
2022-06-11 16:27:39 +0900

    no message

--- nobodycanstopus-b/src/main/java/com/example/nobodycanstopusb/NobodycanstopusBApplication.java
+++ nobodycanstopus-b/src/main/java/com/example/nobodycanstopusb/NobodycanstopusBApplication.java
@@ -3,6 +3,7 @@ package com.example.nobodycanstopusb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//잘되나요
 @SpringBootApplication
 public class NobodycanstopusBApplication {
 
